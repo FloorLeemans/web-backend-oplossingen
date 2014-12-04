7a4469cb6767c51297860a7e073a828235c916f1
FloorLeemans <floorleemans@outlook.com>
2014-12-04 11:55:01 +0100

    Oplossingen 'get' en 'post' toegevoegd

---
+++ opdracht-get/opdracht-get.php
@@ -0,0 +1,157 @@
+<?php
+
+$artikels	=	array(
+					'artikel1'		=>	array(
+											'titel'			=>	"De nieuwe voorwaarden van Facebook: liken of vertrekken",
+											'datum'			=>	"3 december 2014 - 10:48",
+											'inhoud'		=>	"'Voor commercieel gebruik van mijn foto's en teksten op Facebook is te allen tijde mijn schriftelijke toestemming vereist.' Facebookgebruikers claimen deze dagen in boze verklaringen het intellectueel eigendom op alles wat ze op Facebook zetten. De berichten zijn een reactie op de nieuwe gebruikersvoorwaarden die Facebook per 1 januari doorvoert. Hierin staat onder andere dat Facebook gegevens gaat uitwisselen met WhatsApp.</p><p>
+																In 2012 werd het bericht ook al wijd verspreid op Facebook, als reactie op nieuwe voorwaarden. Toen gebruikers deze week een melding en een mail kregen van Facebook over de veranderde voorwaarden, werd het opnieuw opgepikt. De juridische waarde van deze berichten is nihil. Ze zijn - zoals de woordvoerster van Facebook het noemt: een hoax.</p><p>
+																Ook de Nederlandse belangenorganisatie Bits of Freedom noemt de verklaringen 'onzin'. 'Vanaf het moment dat je Facebook gebruikt, ga je akkoord met de voorwaarden. Zo'n tekst doet daar niets aan af', zegt woordvoerster Daphne van der Kroft. De verklaringen moeten volgens haar worden gezien als digitaal equivalent van spandoeken. 'Ze laten zien dat er onvrede is over de manier waarop Facebook omgaat met de privacy van gebruikers.'</p><p>
+																Volgens Facebook is die vrees onterecht. 'Privacy is het allerbelangrijkste goed dat Facebook heeft. Dat komt op plaats 1, 2, 3, 4, 5 en dan een hele tijd niets en dan komen andere dingen. Mensen moeten zich veilig voelen op Facebook', zegt de woordvoerster. 'Zelf controle kunnen hebben over wat je deelt en wat niet en transparantie daarover is essentieel voor het voortbestaan van Facebook.' Hierom besloot Facebook de voorwaarden de helft korter te maken, overzichtelijk te presenteren en de gebruikers voor de verandering eens ruim van tevoren te informeren over de nieuwe voorwaarden via een e-mail en een notificatie op Facebook.</p><p>
+																Hiermee geeft het bedrijf gehoor aan klachten van onder andere een Britse overheidscommissie die stelde dat de gebruikersvoorwaarden ook voor gewone mensen te lezen moeten zijn. 'Het is makkelijker om Shakespeare te doorgronden dan de gemiddelde gebruikersvoorwaarden. Hierom leest niemand ze', zei de voorzitter vorige week. Of zoals comedian John Oliver het probleem schetste: 'Je kunt de volledige tekst van Hitler's 'Mein Kampf' in de gebruikersvoorwaarden zetten en mensen klikken nog steeds blindelings op 'accepteren'.'</p><p>
+																Cosmetisch</p><p>
+																'De verbetering is vooral cosmetisch, er blijft nog steeds veel onduidelijk over wat Facebook met de gegevens doet', zegt Van der Kroft. 'Zo staat er over het samenwerken met andere Facebook-bedrijven als WhatsApp en Instagram: 'We kunnen gegevens over jou met deze bedrijven delen om het ondersteunen en het integreren van de activiteiten van deze bedrijven te vergemakkelijken en onze diensten te verbeteren'. Deze bewoording is supervaag en hiermee houdt Facebook alle opties open. Bij de overname van WhatsApp vreesden gebruikers precies hiervoor.'</p><p>
+																Volgens de Facebook-woordvoerster is er niets veranderd aan de afspraken die Facebook heeft gemaakt met deze bedrijven. 'Wat Facebook hiermee wil is dat je een naadloze overgang hebt tussen de verschillende platforms als Instagram en Facebook bijvoorbeeld. Maar bijvoorbeeld ook dat wanneer je je inlog van Instagram niet meer weet, je alsnog via Facebook kunt inloggen bijvoorbeeld.' </p><p>
+																Woede is er ook om het gebruiken van locatiedata door Facebook. 'Straks weet Facebook de hele tijd waar je bent', zegt de Britse privacy-expert Maninder Gill tegen Vice. Maar dat kon Facebook al, het staat nu alleen duidelijker in de voorwaarden. 'In de nieuwe gebruikersvoorwaarden zijn de verschillende onderdelen met elkaar gecombineerd waarin het gaat over verschillende soorten van informatie die Facebook ontvangt en gebruikt als iemand zijn locatiegegevens deelt. Er is nu meer aandacht voor details en Facebook deelt er nieuwe voorbeelden van hoe het de locatie ontvangt, inclusief Wifi en bluetooth', aldus Facebook.</p><p>
+																's Werelds grootste sociale netwerk verzamelt ongelooflijk veel data over gebruikers en is heel lang onduidelijk is geweest over wat er gebeurt met die gegevens. Het is 'like it or leave'. Toch blijven 1,32 miljard gebruikers trouw aan Facebook. 'Het lastige met Facebook is: al je vrienden zitten er en als je ergens anders naartoe wilt, kun je je profiel niet meenemen. Maar toch is het geen slecht idee om andere sociale netwerken uit te proberen', zegt Van der Kroft. 'Over ieder sociaal netwerk kun je goede en slechte dingen zeggen, maar het belangrijkste is dat er weer gezonde concurrentie op de markt komt. Pas dan kunnen we Facebook dwingen echt te veranderen.'</p><p>
+																(De Morgen)",
+											'afbeelding'	=>	"images/facebook.jpg",
+											'afbeeldingBeschrijving'	=>	"Facebook 'like' ©Belga"
+										),
+
+					'artikel2'		=>	array(
+											'titel'			=>	"Familie Luc De Vos: 'Koop geen bloemen, maar geef geld aan Natuurpunt'",
+											'datum'			=>	"3 december 2014 - 09:39",
+											'inhoud'		=>	"De familie van Luc De Vos vraagt om voor de afscheidsplechtigheid van de muzikant geen bloemen mee te brengen, maar een vrijblijvende bijdrage te doen aan Natuurpunt voor het natuurgebied de Gentbrugse Meersen. Dat staat te lezen in het overlijdensbericht van de Gorki-frontman die zaterdag begraven wordt.</p><p>
+																Luc De Vos overleed op 52-jarige leeftijd aan een 'acuut falen van een aantal vitale organen'. Zijn lichaam werd vorige week zaterdag aangetroffen in zijn werkappartement aan de Gentse Kasteellaan. De afscheidsviering vindt zaterdag plaats om 11 uur in de kerk Onze-Lieve-Vrouw Sint-Pieters op het Sint-Pietersplein in Gent.</p><p>
+																'Muziek maken is geen werk, het is een privilege...', opent het woensdag verschenen overlijdensbericht. 'De families De Vos, Heylen en Gorki' nodigen iedereen uit om afscheid te nemen in de kerk. De Vos wordt daarna in intieme kring begraven. 'Liever geen bloemen, maar een vrijblijvende bijdrage op rekening BE56 2930 2120 7588 van Natuurpunt vzw, met vermelding 'Gentbrugse Meersen - Luc De Vos'.'</p><p>
+																De Gentbrugse Meersen is een gebied van ongeveer 240 hectare in de Gentse deelgemeente dat de komende jaren wordt ingericht als groenpool.</p><p>
+																(De Standaard)",
+											'afbeelding'	=>	"images/lucdevos.jpg",
+											'afbeeldingBeschrijving'	=>	"Luc De Vos op het podium ©Jan Vens"
+										),
+
+					'artikel3'		=>	array(
+											'titel' 		=>	"Binnenkort brengen we blikjes terug naar de winkel",
+											'datum'			=>	"3 december 2014 - 08:00",
+											'inhoud'		=>	"Het is de Vlaamse regering menens over statiegeld op drankblikjes en plastic flessen. Vlaams minister van Leefmilieu Joke Schauvliege (CD&V) heeft een studie­bureau aangesteld dat de maatregel tot in het detail moet uitwerken.</p><p>
+																Het opruimen van zwerfvuil langs onze wegen kost de overheid – lees: de belastingbetaler – veel geld. Daarom wil de nieuwe Vlaamse regering komaf maken met rondslingerend vuilnis, onder meer door een systeem van statiegeld op drankblikjes en (plastic) flessen in te voeren. Concreet betaal je als consument dan iets meer voor een blikje frisdrank, maar krijg je die meerprijs terug als je de lege drankverpakking opnieuw inlevert.</p><p>
+																Het voornemen van de Vlaamse regering botste afgelopen zomer meteen op veel kritiek. Onder meer zelfstandigenorganisatie Unizo en Comeos, de federatie voor handel en diensten, vrezen dat zo’n systeem de bedrijven veel geld zal kosten. Maar de Vlaamse regering zet nu toch duidelijk door. Er is zopas door minister van Leefmilieu Joke Schauvliege (CD&V) een studiebureau aangesteld dat de maatregel de komende weken en maanden gedetailleerd uitwerkt.</p><p>
+																(Gazet van Antwerpen)",
+											'afbeelding'	=>	"images/blikjes.jpg",
+											'afbeeldingBeschrijving'	=>	"Coca-cola-blikjes ©Mia Uydens"
+										),
+					);
+
+$artikelAlleen	=	false;
+
+if (isset($_GET['id'])) 
+{
+	$id 			=	$_GET['id'];
+	$artikels 		= 	array($artikels[$id]);
+	$artikelAlleen	=	true;
+}
+
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <?php if (!$artikelAlleen): ?>
+        	<title>De verzamelkrant</title>
+    		<?php else: ?>
+    			<title><?= $artikels[0]['titel'] ?></title>
+    	<?php endif ?>
+
+        <style>
+
+        .overzicht {
+        	float:left;
+        	width:300px;
+        	margin:35px 25px;
+        	padding:10px;
+        	background-color: grey;
+        }
+
+        .artikel {
+        	width:600px;
+        	margin:35px 25px;
+        	background-color:grey;
+        	padding:10px;
+        }
+
+        h1, p {
+        	font-family:sans-serif;
+        	color:white;
+        }
+		
+		h1 {
+			margin-top:0;
+			font-weight:bold;
+			font-size:20px;
+			padding-bottom:5px;
+			border-bottom:solid white 1px;
+		}
+
+		.titel {
+			font-size: 30px;
+			color:grey;
+			margin-left: 25px;
+			margin-top:10px;
+			margin-bottom:0px;
+			border-bottom:solid grey 2px;
+			text-transform: uppercase;
+		}
+
+		p {
+			font-size:14px;
+		}
+
+
+		a {
+			text-decoration: underline;
+			color:white;
+		}
+
+		a:hover {
+			color:lightgrey;
+		}
+
+		.right{
+			text-align:right;
+			margin-bottom:0;
+		}
+
+		.overzicht img {
+			width:100%;
+		}
+
+		.artikel img {
+			width:300px;
+			float:right;
+			margin-left:10px;
+			margin-bottom:10px;
+		}
+
+        </style>
+
+    </head>
+    <body>
+    	<h1 class='titel'>De verzamelkrant</h1>
+    	<!-- Je moet hier 'as $id => $value' zetten omdat je hier zo de $id definieert zodat $_GET weet uit welke array hij de keys moet halen -->
+	    <?php foreach ($artikels as $id => $value): ?>
+	        <div class="<?= (!$artikelAlleen) ? 'overzicht': 'artikel'; ?>">
+	        	<h1><?= $value["titel"] ?></h1>
+	        	<p><?= $value["datum"] ?></p>
+	        	<img src="<?= $value["afbeelding"] ?>" alt="<?= $value["afbeeldingBeschrijving"] ?>">
+	        	<p><?= (!$artikelAlleen) ? mb_strimwidth($value['inhoud'],0,50,'...'): $value['inhoud']; ?></p>
+	        	<p class="right">
+	        		<?php if (!$artikelAlleen): ?>
+	        			<a href="opdracht-get.php?id=<?= $id ?>">Lees meer ></a>
+	        		<?php endif ?>
+	        	</p>
+	        </div>
+	   	<?php endforeach ?>
+    </body>
+</html>
